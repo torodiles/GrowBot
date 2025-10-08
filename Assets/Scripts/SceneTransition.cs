f40ec351596935acd5d65ecce81c9b8240e19313
torodiles <garrentanavaro2@gmail.com>
2025-10-09 00:06:59 +0700

    GrowBot ver.1.0.4
    - redesigned main menu screen
    - added fade between scene transitions to make it smooth

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,19 +1,40 @@
 using System.Collections;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
 
 public class SceneTransition : MonoBehaviour
 {
-    [SerializeField] private float timeDelay;
+    public static SceneTransition instance;
+
+    [SerializeField] private Animator transitionAnimator;
+    [SerializeField] private float transitionTime = 1f;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public void MoveToScene(string sceneName)
     {
-        StartCoroutine(delayTransition(sceneName, timeDelay));
+        StartCoroutine(LoadSceneWithFade(sceneName));
     }
 
-    IEnumerator delayTransition(string sceneName, float timeDelay)
+    IEnumerator LoadSceneWithFade(string sceneName)
     {
-        yield return new WaitForSeconds(timeDelay);
+        transitionAnimator.SetTrigger("End");
+
+        yield return new WaitForSeconds(transitionTime);
+
         SceneManager.LoadScene(sceneName);
 
+        transitionAnimator.SetTrigger("Start");
     }
 }
